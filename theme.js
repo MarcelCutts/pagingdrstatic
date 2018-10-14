898e6d7009e487324d4dcbe8ad9a13245159c85f
Marcel Cutts <marcel.cutts@gmail.com>
2018-10-14 22:58:13 +0100

    Done

--- theme.js
+++ theme.js
@@ -3,6 +3,19 @@ import theme from "mdx-deck/themes";
 export default {
   ...theme,
   li: {
-    "list-style": "none"
-  }
+    "list-style": "none",
+    margin: "40px 0"
+  },
+  font: "Josefin Slab",
+  colors: {
+    ...theme.colors,
+    background: "#ddcdb5"
+  },
+  h1: {
+    "font-family": "Pacifico",
+    "font-size": "3.5em",
+    "margin-bottom": "25px",
+    "font-weight": "500"
+  },
+  css: { ...theme.css, em: { color: "#6e2f3b" } }
 };
